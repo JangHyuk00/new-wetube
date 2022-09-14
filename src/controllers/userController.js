c7e1e598cacb29d66f4670e4259ab75c35de9e40
JangHyuk Yim <yim0823@hotmail.com>
2022-09-15 07:29:17 +0900

    router
    router and controller for global, users and videos

---
+++ src/controllers/userController.js
@@ -0,0 +1,5 @@
+export const join = (req, res) => res.send("join");
+export const edit = (req, res) => res.send("Edit User");
+export const remove = (req, res) => res.send("remove user");
+
+export default join;
--- src/server.js
+++ src/server.js
@@ -1,26 +1,21 @@
 import express from "express";
 import morgan from "morgan";
 
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+
 const PORT = 4000
 const app = express();
 const logger = morgan("dev");
-
-const privateMiddleware = (req, res, next) => {
-    const url = req.url;
-    if (url === "/protected"){
-        return res.send("<h1>Not allowed</h1>");
-    }
-    next();
-}
-
 app.use(logger);
-app.use(privateMiddleware);
-app.get("/", (res, req) => {
-    req.send("Hi");
-});
-app.get("/protected", (res, req) => {
-    req.send("hey its not protected bro");
-});
+
+
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+app.use("/", globalRouter);
+
+
 
 const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT}`);
 
