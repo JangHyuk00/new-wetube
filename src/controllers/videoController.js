f52138d6cb54971c89621fb271b8a4811696e1bd
JangHyuk Yim <yim0823@hotmail.com>
2022-09-16 22:24:56 +0900

    other routers and regex
    Adding other routers and learned how to pass regular expression to URL

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,3 +1,8 @@
 export const trending = (req, res) => res.send("homepage videos");
-export const watch = (req, res) => res.send("Videos");
+export const see = (req, res) => {
+    return res.send(`Watch Video#${req.params.id}`)
+};
 export const edit = (req, res) => res.send("edit video");
+export const search = (req, res) => res.send("search");
+export const upload = (req, res) => res.send("upload");
+export const deleteVideo = (req, res) => res.send("delete video");
