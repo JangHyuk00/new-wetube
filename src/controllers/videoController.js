cbaef79272bddcc8c9f060f6194e836cfef5c238
JangHyuk Yim <yim0823@hotmail.com>
2022-09-18 23:44:03 +0900

    conditional, mixings and array database

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,29 @@
-export const trending = (req, res) => res.send("homepage videos");
-export const see = (req, res) => {
-    return res.send(`Watch Video#${req.params.id}`)
+let videos = [
+    {
+        title: "the First one",
+        rating: 5,
+        comment: 2,
+        createAt: "10 mins ago",
+        views: 59,
+        id: 1,
+    }, 
+    {
+        title: "the Second one",
+        rating: 5,
+        comment: 2,
+        createAt: "5 mins ago",
+        views: 59,
+        id: 2,
+    }, 
+];
+
+export const trending = (req, res) => { 
+    res.render("home", {pageTitle: "Home", videos});
+};
+export const watch = (req, res) => {
+    const {id} = req.params;
+    const video = videos[id-1];
+    res.render("watch", {pageTitle: `watching ${video.title}`, video});
 };
 export const edit = (req, res) => res.send("edit video");
 export const search = (req, res) => res.send("search");
