7d77b47a3f4bb33a6aecae1062010c1693cbd506
JangHyuk Yim <yim0823@hotmail.com>
2022-09-19 15:32:00 +0900

    edit video
    Learned about POST and how to post a new title and save it

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -25,7 +25,14 @@ export const watch = (req, res) => {
     const video = videos[id-1];
     res.render("watch", {pageTitle: `watching ${video.title}`, video});
 };
-export const edit = (req, res) => res.send("edit video");
-export const search = (req, res) => res.send("search");
-export const upload = (req, res) => res.send("upload");
-export const deleteVideo = (req, res) => res.send("delete video");
+export const getEdit = (req, res) => {
+    const {id} = req.params;
+    const video = videos[id-1];
+    return res.render("edit", {pageTitle: `Editing ${video.title}`, video});
+};
+export const postEdit = (req, res) => {
+    const {id} = req.params;
+    const {title} = req.body;
+    videos[id-1].title = title; 
+    return res.redirect(`/videos/${id}`);
+};
