0cb887126a838fc31d8be2f906b0b0f9cf226f54
JangHyuk Yim <yim0823@hotmail.com>
2022-09-19 19:21:11 +0900

    Practice uploading fake data

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -36,3 +36,18 @@ export const postEdit = (req, res) => {
     videos[id-1].title = title; 
     return res.redirect(`/videos/${id}`);
 };
+export const getUpload = (req, res) => {
+    return res.render("upload");
+};
+export const postUpload = (req, res) => {
+    const newVideo = {
+        title: req.body.title,
+        rating: 0,
+        comment: 0,
+        createAt: "just now",
+        views: 0,
+        id: videos.length + 1 ,
+    };
+    videos.push(newVideo);
+    res.redirect("/");
+}
